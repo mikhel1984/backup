157a3bfd405bdfc6fda4cb4d32630f6b57a5a5a8
Stanislav <mikhel.sk@gmail.com>
2021-12-04 09:11:48 +0300

    Add example

---
+++ example/vc.lua
@@ -0,0 +1,16 @@
+#!/usr/local/bin/lua
+require "backup"
+
+-- file extention
+-- EXT = ".bkp"
+-- save to directory
+DIR = "foo"
+-- file list
+FILES = {
+-- current directory
+"1.txt",
+-- other directories
+["bar/2.txt"] = "bar2.txt",
+}
+
+backup()
